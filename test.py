c9f802880fb51848fae138c0858fd7f03071c55e
AlexanderPezarro <alexanderpezarro@gmail.com>
2022-10-30 19:29:56 +0000

    Fixes

--- test.py
+++ test.py
@@ -33,6 +33,7 @@ SCREEN_WIDTH = pygame.display.get_window_size()[0]
 SCREEN_HEIGHT = pygame.display.get_window_size()[1]
 
 BG = pygame.image.load("assets/Background.png")
+BG = pygame.transform.scale(BG, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # print(gen_walls_array(SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -85,13 +86,13 @@ def main_menu():
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
         MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
-        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+        MENU_RECT = MENU_TEXT.get_rect(center=(SCREEN_WIDTH/2, 100))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), 
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(SCREEN_WIDTH/2, 250), 
                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), 
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(SCREEN_WIDTH/2, 400), 
                             text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550), 
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(SCREEN_WIDTH/2, 550), 
                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
         screen.blit(MENU_TEXT, MENU_RECT)
@@ -178,7 +179,6 @@ def play():
             screen.blit(human_screen, (0,0))
             screen.blit(alien_screen, (SCREEN_WIDTH/2, 0))
             
-        print("yooo")
         clock.tick(30)
         pygame.display.flip()
     
